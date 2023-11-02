4fc0b922b59e5bb8dc81c6d2b1c5dc5d3acd4543
pcastill0 <pedro.castillo@enti.cat>
2023-11-02 14:27:45 +0100

    Cambios en clase

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include<string>
 #include<vector>
 
+#define MAP_SIZE 10
 
-char map[10][10] = {
+enum Scene {INTRO, NAVIGATION, COMBAT, STATUS};
+Scene currentScene = INTRO;
+
+char map[MAP_SIZE][MAP_SIZE] = {
 {'#','#','#','#','#','#','#','#','#','#'},
 {'#','@','.','P','.','P','.','.','.','#'},
 {'#','#','#','G','#','S','#','#','M','#'},
@@ -16,36 +20,91 @@ char map[10][10] = {
 {'#','#','#','#','#','#','#','#','#','#'}};
 
 struct Player {
+	int position[2] = {1,1};
 	std::string name;
 	int hp = 100;
+	int maxHp = 100;
 	int potions = 0;
 	int bombs = 0;
-	int sowrds = 0;
-	int keys = 0;
+	bool swords = 0;
+	bool keys = 0;
 };
 
-bool inCombat = false;
-
-void Intro(Player player&) {
+void Intro(Player & player) {
+	system("cls");
+	currentScene = INTRO;
 	std::cout << "- - - - - - - - - - - - - H E R O S  Q U E S T - - - - - - - - - - - - -\n";
 	std::cout << "lOREM IPSUM lOREM IPSUM lOREM IPSUM lOREM IPSUM lOREM IPSUM lOREM IPSUM\n\n";
 	std::cout << "Use the HELP command to show the options\n";
 	std::cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
 	std::cout << "Name: ";
 	std::cin >> player.name;
-	std::cout << "Lorem ipsum" << player.name <<"\n";
+	std::cout << "Lorem ipsum " << player.name <<"\n";
+
+	currentScene = NAVIGATION;
 }
 
-void Navigation() {
+void Navigation(Player player) {
+	system("cls");
+	bool north = false, south = false, east = false, west = false;
+	int x, y;
+	std::cout << "[" << player.name << "] at [" 
+		<< player.position[0] <<", " << player.position[1]<<"]\n";
+
+	std::cout << "You can go: ";
+
+	if (map[player.position[0]][player.position[1] - 1] != '#') {
+		north = true;
+		std::cout << "[North]";
+	}
+
+	if (map[player.position[0]][player.position[1] + 1] != '#') {
+		south = true;
+		std::cout << "[South]";
+
+	}
+
+	if (map[player.position[0]+1] [player.position[1]] != '#') {
+		east = true;
+		std::cout << "[East]";
+
+	}
+
+	if (map[player.position[0] -1][player.position[1]] != '#') {
+		west = true;
+		std::cout << "[West]";
+
+	}
+
+	std::cout << "\nWhat will you do?";
+
+	std::cin >> x;
+	std::cin >> y;
 
 }
 
 void Combat() {
+	system("cls");
 
 }
 
 void PickupItem() {
 
+}
+
+void Status(Player player) {
+	system("cls");
+	std::cout << "------------ PLAYER ------------\n";
+	std::cout << player.name <<"\n";
+	std::cout << "-------------------------------\n";
+	std::cout << "HP: " << player.hp << "/" << player.maxHp << "\n";
+	std::cout << "-----------INVENTORY-----------\n";
+	std::cout << "Potion: " << player.potions << "\n";
+	std::cout << "Bomb: " << player.bombs << "\n";
+	std::cout << "Sword: " << player.swords << "\n";
+	std::cout << "Key: " << player.keys << "\n";
+	std::cout << "--------------------------------\n";
+	currentScene = NAVIGATION;
 }
 
 int main() {
@@ -61,13 +120,17 @@ int main() {
 	while (true){
 		Player player;
 
-		Intro(player);
-
-		while (player.hp > 0){
-			if (!inCombat) {
-				Navigation();
-			}
+		if (currentScene == INTRO) {
+			Intro(player);
+		} else if (currentScene == NAVIGATION) {
+			Navigation(player);
+		} else if (currentScene == COMBAT) {
+			Combat();
+		} else if (currentScene == STATUS) {
+			Status(player);
 		}
+
+		system("pause");
 
 	}
 
